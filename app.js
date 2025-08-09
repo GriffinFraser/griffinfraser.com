0ff7832596d5e00d7f077dd014e2d5386465e8de
GriffinFraser <griffinfraser101@gmail.com>
2025-08-09 21:19:50 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('#normal-navbar')
+const menuMain = document.querySelector('main')
 
 menu.addEventListener('click', function(){
     menu.classList.toggle('active1');
     menuLinks.classList.toggle('active2');
+    menuMain.classList.toggle('active3');
+});
+
+const contacts = document.querySelector('#mobile-contacts')
+const contactsLinks = document.querySelector('#normal-contacts')
+
+contacts.addEventListener('click', function(){
+    contacts.classList.toggle('active4');
+    contactsLinks.classList.toggle('active5');
 });
